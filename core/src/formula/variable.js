5a1ed84e7a464c1a5fc526bbed53297f5d8cf594
Željko Brcković <Zeljko.Brckovic1@infobip.com>
2022-12-04 15:36:09 +0100

    Added models for variables

---
+++ core/src/formula/variable.js
@@ -0,0 +1,44 @@
+/**
+ * Variable
+ * @param id - The unique identifier of this variable inside some context.
+ * @constructor
+ */
+function Var({id}) {
+    this.id = id
+}
+
+Object.assign(Var.prototype, {
+    toString() {
+        return this.id
+    },
+    with(changes = {}) {
+        return new this.constructor({...this, ...changes})
+    }
+})
+
+
+/**
+ * Individual variable
+ * @constructor
+ */
+function IndVar({id}) {
+    Var.call(this, {id})
+}
+
+Object.assign(IndVar.prototype, Var.prototype)
+
+
+/**
+ * Predicate variable
+ * @property arity - The number of terms required to form an atomic variable
+ * with this predicate variable. When `arity` is zero, the predicate variable is
+ * actually a "propositional variable" - it can stand on its own to form an
+ * atomic formula.
+ * @constructor
+ */
+function PredVar({id, arity = 0} = {}) {
+    Var.call(this, {id})
+    this.arity = arity
+}
+
+Object.assign(PredVar.prototype, Var.prototype)
