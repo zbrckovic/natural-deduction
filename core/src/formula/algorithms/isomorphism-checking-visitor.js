e305a3c07d7a6eb25cdca9612036348326184a60
Željko Brcković <Zeljko.Brckovic1@infobip.com>
2022-12-24 16:40:55 +0100

    Refactoring

--- core/src/formula/algorithms/isomorphism-checking-visitor.js
+++ core/src/formula/algorithms/isomorphism-checking-visitor.js
@@ -1,7 +1,14 @@
-/** Creates an expression visitor which checks whether this and other expression are isomorphic. */
+/**
+ * Creates an expression visitor which checks whether this and other expression are isomorphic.
+ * @param formula - The reference formula which will be checked for isomorphism against the visited
+ * formula. Isomorphism is a symmetric relation, so it is not important which formula is the
+ * reference formula and which is the visited one.
+ */
 export function createIsomorphismCheckingVisitor (formula) {
   const that = Object.create(isomorphismCheckingVisitorTrait)
-  that._formula = formula
+  that._refFormula = formula
+  /** Bijection between reference formula variables and visited formula variables. */
+  that._varMap = {}
   return that
 }
 
